97019d9d8e1934311883dc0e3636a4f3086a2b9a
wxsm <wxsms@foxmail.com>
2023-10-25 22:47:37 +0800

    u

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,13 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -20,7 +23,7 @@ struct State {
     color: (u8, u8, u8),
     position: Point,
     quit: bool,
-    message: String
+    message: String,
 }
 
 impl State {
@@ -43,6 +46,20 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::ChangeColor(a, b, c) => {
+                self.color = (a, b, c);
+            }
+            Message::Echo(msg) => {
+                self.message = msg;
+            }
+            Message::Move(point) => {
+                self.position = point;
+            }
+            Message::Quit => {
+                self.quit = true;
+            }
+        }
     }
 }
 
