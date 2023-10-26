599ad6a8159e7c3d1d7a18a1343ef8c863bbdb6c
guokairui <wxsms@foxmail.com>
2023-10-26 16:56:00 +0800

    u

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,18 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut v = self[..].to_vec();
+        v.push("Bar".into());
+        v
+    }
+}
 
 #[cfg(test)]
 mod tests {
